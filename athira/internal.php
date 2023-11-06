fd5e6f866885d7ce7f0c8bb810fbaecbe3377a59
athirathekkeveettil <147636246+athirathekkeveettil@users.noreply.github.com>
2023-11-06 11:37:02 +0530

    Add files via upload

---
+++ athira/internal.php
@@ -0,0 +1,58 @@
+<html>
+   <head>
+       <title>INTERNAL MARKS</title>
+   </head>
+    <body>
+    <br><br>
+     <style>
+       table{
+	           border:1px solid black;
+                   width:500px;
+	    }
+	    
+      th,td{
+	           padding:10px;
+	           border:1px solid black;
+	           text-align:center;
+	   }
+      
+    </style>
+    <center>
+    
+    <table>
+           <form method="post">
+           <tr>
+             <td>ROLL NO.</td>
+             <td><input type="text" id="t1"></td>
+           </tr>
+           <tr>
+             <td>Name</td>
+             <td><input type="text" id="t2"></td>
+           </tr>
+           
+           <tr>
+             <td colspan="2"><center><input type="submit" name="sub" value="MARKLIST"></center></td>
+            </tr>
+          </form>
+     </table>
+   </center>
+
+<html>
+	<body>
+	<CENTER>
+	
+	<h2><u>INTERNAL MARK</u></h2>
+		<table border="">
+					<tr>
+						<th>SI.NO</th>
+						<th>Sub 1</th>
+						<th>Sub 2</th>
+						<th>Sub 3</th>
+						<th>TOTAL</th>
+						<th>P/F</th>
+					</tr>
+				
+ 
+   
+ </body>
+</html>
